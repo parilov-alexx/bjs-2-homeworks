9ae931aaa65080cd884b8f44a4bcdab00a929776
parilov alexx <parilov_alexx@mail.ru>
2023-04-02 13:26:53 +0300

    Первый пункт задания выполнен

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  return {
+    name,
+    gender,
+    age,
+    marks: []
+   }
 }
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+this.subject = subjectName;
+};
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if(marks in Student === true) {
+this.marks = marks;
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if(marks in Student === true) {
+        for(let i = 0; i < Student.marks.length; i++) {
+            let res = 0;
+            res = res + Student.marks[i] / Student.marks.length;
+            return res;
+        }
+    } else {return 0;}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
 }
