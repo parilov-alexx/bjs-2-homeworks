8aeb6e46d203ac35fb360264ed9308163467fed5
parilov alexx <parilov_alexx@mail.ru>
2023-04-03 12:42:44 +0300

    Задание полностью выполнено

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,35 +1,35 @@
 function Student(name, gender, age) {
-  return {
-    name,
-    gender,
-    age,
-    marks: []
-   }
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
 }
 
 
-Student.prototype.setSubject = function (subjectName) {
-this.subject = subjectName;
+Student.prototype.setSubject = function(subjectName) {
+	this.subject = subjectName;
 };
 
-Student.prototype.addMarks = function (...marks) {
-  if(marks in Student === true) {
-this.marks = marks;
-  }
+Student.prototype.addMarks = function(...marks) {
+	if ("marks" in this === true) {
+		this.marks.push(...marks);
+	}
 }
 
-Student.prototype.getAverage = function () {
-    if(marks in Student === true) {
-        for(let i = 0; i < Student.marks.length; i++) {
-            let res = 0;
-            res = res + Student.marks[i] / Student.marks.length;
-            return res;
-        }
-    } else {return 0;}
+Student.prototype.getAverage = function() {
+	if ("marks" in this === true) {
+		let res = 0;
+		for (let i = 0; i < this.marks.length; i++) {
+			res = res + this.marks[i] / this.marks.length;
+		}
+		return res;
+	} else {
+		return 0;
+	}
 }
 
-Student.prototype.exclude = function (reason) {
-  this.excluded = reason;
-  delete this.subject;
-  delete this.marks;
+Student.prototype.exclude = function(reason) {
+	this.excluded = reason;
+	delete this.subject;
+	delete this.marks;
 }
